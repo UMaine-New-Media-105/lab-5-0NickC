fea22f517a7a4e7a61a03a24cb5275e9525f486c
0NickC <124187302+0NickC@users.noreply.github.com>
2023-03-01 12:29:05 -0500

    Made code to create sprite that bounces around
    https://editor.p5js.org/Nick_C/sketches/wwaPFIpfB

---
+++ lab5ex1.js
@@ -0,0 +1,45 @@
+let x;
+let y;
+let speedX;
+let speedY;
+
+function setup() {
+  createCanvas(400, 400);
+
+  x = 13;
+  y = 13;
+  speedX = random(1, 5);
+  speedY = random(1, 5);
+}
+
+function draw() {
+  background("palevioletred");
+
+  x = x + speedX;
+  y = y + speedY;
+
+  addOk(x, y, 0.5);
+
+  if (x > 265 || x < -50) speedX = speedX * -1;
+
+  if (y > 205 || y < -65) speedY = speedY * -1;
+}
+
+function addOk(x, y, size) {
+  push();
+  translate(x, y);
+  scale(size);
+  fill("mistyrose");
+  rect(175, 250, 50);
+  ellipse(200, 200, 100, 140);
+  fill("moccasin");
+  rect(125, 300, 150, 100);
+  fill("lavender");
+  quad(150, 280, 250, 280, 275, 300, 125, 300);
+  rect(190, 300, 20, 100);
+  triangle(125, 300, 100, 400, 125, 400);
+  fill("silver");
+  rect(195, 300, 10, 100);
+  pop();
+}
+
